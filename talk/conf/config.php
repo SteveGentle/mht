980e5fd2288d7e08d7a0ec8cbe41a6ab097ad3ab
Steve Gentle <steve.gentle.cchmc@gmail.com>
2015-03-29 19:47:13 -0400

    troubleshooting tweaks

---
+++ talk/conf/config.php
@@ -0,0 +1,46 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Conversations
+$Configuration['Conversations']['Version'] = '2.1.9';
+
+// Database
+$Configuration['Database']['Name'] = 'mht5_vf219';
+$Configuration['Database']['Host'] = 'localhost';
+$Configuration['Database']['User'] = 'root';
+$Configuration['Database']['Password'] = 'root';
+
+// EnabledApplications
+$Configuration['EnabledApplications']['Conversations'] = 'conversations';
+$Configuration['EnabledApplications']['Vanilla'] = 'vanilla';
+
+// EnabledPlugins
+$Configuration['EnabledPlugins']['GettingStarted'] = 'GettingStarted';
+$Configuration['EnabledPlugins']['HtmLawed'] = 'HtmLawed';
+
+// Garden
+$Configuration['Garden']['Title'] = 'Miami Hawk Talk';
+$Configuration['Garden']['Cookie']['Salt'] = 'X6ZKXKRVHS';
+$Configuration['Garden']['Cookie']['Domain'] = '';
+$Configuration['Garden']['Registration']['ConfirmEmail'] = TRUE;
+$Configuration['Garden']['Email']['SupportName'] = 'Miami Hawk Talk';
+$Configuration['Garden']['InputFormatter'] = 'Html';
+$Configuration['Garden']['Html']['SafeStyles'] = TRUE;
+$Configuration['Garden']['Version'] = '2.1.9';
+$Configuration['Garden']['RewriteUrls'] = TRUE;
+$Configuration['Garden']['Cdns']['Disable'] = FALSE;
+$Configuration['Garden']['CanProcessImages'] = TRUE;
+$Configuration['Garden']['SystemUserID'] = '2';
+$Configuration['Garden']['Installed'] = TRUE;
+
+// Plugins
+$Configuration['Plugins']['GettingStarted']['Dashboard'] = '1';
+
+// Routes
+$Configuration['Routes']['DefaultController'] = 'discussions';
+
+// Vanilla
+$Configuration['Vanilla']['Version'] = '2.1.9';
+
+// Last edited by Steve (127.0.0.1)2015-03-29 23:39:36
+
+//$Configuration ['Debug'] = TRUE;
